d75eb19e4876c466df23b4b2315b98f87b7a7dc0
ErnestYIP <eyipcm@gmail.com>
2025-11-02 02:52:22 +0800

    Update LICENSE info

--- m2py/bin/compareGraph.bat
+++ m2py/bin/compareGraph.bat
@@ -1,6 +1,22 @@
 @echo off
-REM HMM Stock Market Prediction - Graph Comparison Batch File
-REM This batch file runs the comparison script with absolute paths
+:: HMM Stock Market Prediction - Graph Comparison Batch File
+:: This batch file runs the comparison script with absolute paths
+
+:: SPDX-FileCopyrightText: Copyright (C) 2025 Ernest YIP <eyipcm@gmail.com>
+:: SPDX-License-Identifier: GPL-3.0-or-later
+
+:: This program is free software: you can redistribute it and/or modify
+:: it under the terms of the GNU General Public License as published by
+::the Free Software Foundation, either version 3 of the License, or
+::(at your option) any later version.
+
+:: This program is distributed in the hope that it will be useful,
+:: but WITHOUT ANY WARRANTY; without even the implied warranty of
+:: MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+:: GNU General Public License for more details.
+
+:: You should have received a copy of the GNU General Public License
+:: along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 echo ============================================================
 echo HMM Stock Market Prediction - Python vs MATLAB Comparison
--- m2py/utils/plot_comparison.py
+++ m2py/utils/plot_comparison.py
@@ -1,6 +1,22 @@
 """
 Plot comparison utilities for HMM Stock Market Prediction
 Compares Python-generated plots with MATLAB-generated plots
+
+SPDX-FileCopyrightText: Copyright (C) 2025 Ernest YIP <eyipcm@gmail.com>
+SPDX-License-Identifier: GPL-3.0-or-later
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import os
